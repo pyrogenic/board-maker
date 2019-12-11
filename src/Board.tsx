b6a58629eabc6bfb69b595376ca870b935290d03
Joshua Pollak <abottomplesspit@gmail.com>
2019-12-11 14:32:01 -0800

    board

---
+++ src/Board.tsx
@@ -0,0 +1,97 @@
+import React from 'react';
+import SlideGrid, { ISlideGridTuning } from '@pyrogenic/slide-grid/lib/SlideGrid';
+import '@pyrogenic/slide-grid/lib/SlideGrid.css';
+import sample from 'lodash/sample';
+
+export type TileType = 'empty' | 'blocker' | 'double-letter' | 'triple-letter' | 'double-word' | 'triple-word' | 'start';
+
+export const TileTypes: TileType[] = [
+    'empty',
+    'blocker',
+    'double-letter',
+    'triple-letter',
+    'double-word',
+    'triple-word',
+    'start',
+];
+
+export interface ITile {
+    id: string;
+    type: TileType;
+}
+
+export interface IBoardState {
+    tiles: ITile[];
+}
+
+export interface IBoardProps {
+    tuning: ISlideGridTuning;
+    tiles: ITile[];
+    height: number;
+    width: number;
+
+    onChange?: (tiles: ITile[]) => void;
+};
+
+export default class Board extends React.Component<IBoardProps, IBoardState> {
+    protected tap?: ((key: string) => void) = (key) => {
+        const tile = this.getTileById(key);
+        let type = tile?.type;
+        const index = TileTypes.indexOf(type);
+        tile.type = TileTypes[(index + 1) % TileTypes.length];
+        this.setState(({tiles}) => ({tiles: [...tiles]}));
+    }
+
+    protected smear?: ((key: string) => void);
+
+    constructor(props: Readonly<IBoardProps>) {
+        super(props);
+        this.state = {tiles: this.props.tiles};
+    }
+
+    public componentDidUpdate() {
+        this.props.onChange?.(this.state.tiles);
+    }
+
+    public render() {
+        const { tuning } = this.props;
+        return <div className="board">
+            <SlideGrid tuning={tuning} exchange={this.exchange} canExchange={this.canExchange} tap={this.tap} smear={this.smear}>
+                {this.state.tiles.map((tile) => <div className={"tile " + tile.type} key={tile.id} id={tile.id}/>)}
+            </SlideGrid>
+        </div>;
+    }
+
+    protected canExchange?(a: string, b?: string): boolean | number;
+
+    protected getTileById(id: string) {
+        return this.state.tiles.find((e) => e.id === id)!;
+    }
+
+    protected getTileIndexById(id: string) {
+        return this.state.tiles.findIndex((e) => e.id === id)!;
+    }
+
+    protected renderTileContent(tile: ITile) {
+        return <div className={tile.type} />;
+    }
+
+    protected exchange = (a: string, b: string) => {
+        this.setState((state) => {
+            const ai = state.tiles.findIndex((e) => e.id === a);
+            const bi = state.tiles.findIndex((e) => e.id === b);
+            const tiles: ITile[] = [];
+            state.tiles.forEach((tile, i) => {
+                if (i === ai) {
+                    tiles[bi] = tile;
+                } else if (i === bi) {
+                    tiles[ai] = tile;
+                } else {
+                    tiles[i] = tile;
+                }
+            });
+            return { tiles };
+        })
+    }
+
+}
