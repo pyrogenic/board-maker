bf72ac174d138eb53bad1d120d56c29d4d38c4b1
Joshua Pollak <abottomplesspit@gmail.com>
2019-12-11 16:35:21 -0800

    Updates

---
+++ precache-manifest.b2fff3c3071b5e301be8c736381af8ca.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39df6416b1ae97368e199e25a8ce2bed",
+    "url": "/board-maker/index.html"
+  },
+  {
+    "revision": "84938056cc8164979a28",
+    "url": "/board-maker/static/css/2.eef82d55.chunk.css"
+  },
+  {
+    "revision": "023541154169573a7ee3",
+    "url": "/board-maker/static/css/main.8aa44099.chunk.css"
+  },
+  {
+    "revision": "84938056cc8164979a28",
+    "url": "/board-maker/static/js/2.49f83dfe.chunk.js"
+  },
+  {
+    "revision": "e4b09c039970332d74e4fba3bd634172",
+    "url": "/board-maker/static/js/2.49f83dfe.chunk.js.LICENSE"
+  },
+  {
+    "revision": "023541154169573a7ee3",
+    "url": "/board-maker/static/js/main.d63d1995.chunk.js"
+  },
+  {
+    "revision": "89dda89837a8b5c7f84d",
+    "url": "/board-maker/static/js/runtime-main.366d1d75.js"
+  }
+]);
